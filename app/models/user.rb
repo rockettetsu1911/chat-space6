6f1c3ef9f0af7321c0218ff27472c5ba3aba2f16
rockettetsu1911 <rockettetsu@gmail.com>
2020-04-14 13:04:52 +0900

    create_search

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_many :messages
 
+
+  def self.search(input, id)
+    return nil if input == ""
+    User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+  end
          
   validates :name, presence: true, uniqueness: true
 end
